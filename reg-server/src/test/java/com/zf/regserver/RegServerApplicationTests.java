f67f8643aff6d665b104c9b1401914e6a0bc86c3
zhangfan <>
2020-06-02 21:57:24 +0800

    zf

---
+++ reg-server/src/test/java/com/zf/regserver/RegServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zf.regserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
